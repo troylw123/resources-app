e1e5d41d8fad5a73833f3c60b596f8140905fb4d
Troy Weaver <troylw@gmail.com>
2022-09-21 16:04:33 -0400

    Registration Email Working

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const { userRegisterValidator } = require("../validators/auth");
+const { runValidation } = require("../validators");
 
 // import from controllers
 const { register } = require("../controllers/auth");
 
-router.get("/register", register);
+router.post("/register", userRegisterValidator, runValidation, register);
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,11 +1,27 @@
 const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
+
+//db
+mongoose
+  .connect(process.env.DATABASE_CLOUD, {})
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.log("DB Error => ", err));
 
 //import routes
 const authRoutes = require("./routes/auth");
 
 //middleware
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+// uses cors to only allow access from one domain
+app.use(cors({ origin: process.env.CLIENT_URL }));
+
 app.use("/api", authRoutes);
 
 const port = process.env.PORT || 8000;
